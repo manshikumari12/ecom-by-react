e32622452b72f2efcefaee10d64188177dd65fd2
manshikumari <manshisbp@gmail.com>
2024-09-29 06:52:44 -0700

    done

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,71 +1,7 @@
 const jwt = require("jsonwebtoken");
 const { blacklistModel } = require("../model/black.list.model");
 
-// const auth = (req, res, next) => {
-//     const token = req.headers.authorization;
-//     console.log(token)
 
-//     if (!token) {
-//         return res.status(401).json({ msg: "Token Missing" });
-//     }
-
-//     jwt.verify(token, "masai", (err, decoded) => {
-//         if (err) {
-//             return res.status(401).json({ msg: "Invalid Token" });
-//         }
-
-//         req.user = {
-//             userid: decoded.userid,
-//             name: decoded.name,
-//             email: decoded.email,
-          
-//         };
-//         next();
-//     });
-// };
-
-
-
-// module.exports={auth}
-
-// const jwt = require("jsonwebtoken");
-// const { blacklistModel } = require("../model/black.list.model");
-
-// const auth = async (req, res, next) => {
-//     const token = req.headers.authorization;
-//     // console.log(token)
-
-//     if (!token) {
-//         return res.status(401).json({ msg: "Token Missing" });
-//     }
-
-//     try {
-//         const tokenInBlacklist = await blacklistModel.findOne({ token });
-//         console.log(tokenInBlacklist)
-
-//         if (tokenInBlacklist) {
-//             return res.status(401).json({ msg: "Token Blacklisted" });
-//         }
-
-//         jwt.verify(token, "masai", (err, decoded) => {
-//             if (err) {
-//                 return res.status(401).json({ msg: "Invalid Token" });
-//             }
-
-//             req.user = {
-//                 userid: decoded.userid,
-//                 name: decoded.name,
-//                 email: decoded.email,
-//             };
-
-//             next();
-//         });
-//     } catch (error) {
-//         return res.status(500).json({ msg: "Internal Server Error", error: error.message });
-//     }
-// };
-
-// module.exports = { auth };
 
 const auth = async (req, res, next) => {
     const token = req.headers.authorization;
